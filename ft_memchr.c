14e18ccd0cf856c95dad60b81e7432da7c2604fe
hmouhib <hmouhib@student.42.fr>
2023-10-02 04:51:28 +0100

    wa mok hhh

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: hmouhib <hmouhib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 04:15:00 by hmouhib           #+#    #+#             */
-/*   Updated: 2023/10/02 03:43:27 by hmouhib          ###   ########.fr       */
+/*   Updated: 2023/10/02 04:51:20 by hmouhib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@
 void    *ft_memchr(const void *s, int c, size_t n)
 {
     size_t i;
+    unsigned char   *_s;
 
     i = 0;
+    _s = (unsigned char *)s;
     while (i < n)
     {
-        if ((unsigned char)s[i] == c)
-            return (&s[i]);
-        i += sizeof(s[i]);
+        if (_s[i] == c)
+            return (&_s[i]);
+        i += sizeof(_s[i]);
     }
 }
