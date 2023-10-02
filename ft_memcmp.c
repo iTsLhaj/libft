09b57c4052aa28c06b066c19eda311ab238e4fb2
hmouhib <hmouhib@student.42.fr>
2023-10-02 05:04:35 +0100

    mok 2

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hmouhib <hmouhib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 04:20:44 by hmouhib           #+#    #+#             */
-/*   Updated: 2023/10/02 03:43:27 by hmouhib          ###   ########.fr       */
+/*   Updated: 2023/10/02 05:04:25 by hmouhib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,18 @@
 
 void    *ft_memcmp(void *dest, const void *src, size_t n)
 {
-    size_t i;
+    unsigned char       *_dest;
+    const unsigned char *_src;
+    size_t              i;
 
     i = 0;
-    while (dest[i] && src[i] && i < n)
+    _dest = (unsigned char *)dest;
+    _src = (const unsigned char *)src;
+    while (_dest[i] && _src[i] && i < n)
     {
-        if (dest[i] != src[i])
-            return (dest[i] - src[i]);
-        i += sizeof(src[i]);
+        if (_dest[i] != _src[i])
+            return (_dest[i] - _src[i]);
+        i += sizeof(_src[i]);
     }
-    return (dest[i] - src[i]);
+    return (_dest[i] - _src[i]);
 }
--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: hmouhib <hmouhib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 04:51:14 by hmouhib           #+#    #+#             */
-/*   Updated: 2023/10/02 04:49:03 by hmouhib          ###   ########.fr       */
+/*   Updated: 2023/10/02 05:02:53 by hmouhib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char    *ft_strstr(const char *haystack, const char *needle)
     while (haystack[i])
     {
         ph = (void *)&haystack[i];
-        if (ft_memcmp(ph, needle, sizeof(haystack[i])) == 0)
+        if (ft_memcmp(ph, needle, ft_strlen(haystack)) == 0)
             return (&haystack[i]);
         i++;
     }
