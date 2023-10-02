a2fc4bbc5b3c32736f2021df2d00e5319eccbcd4
hmouhib <hmouhib@student.42.fr>
2023-10-02 04:52:53 +0100

    same sh*t yawmiyan

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: hmouhib <hmouhib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 03:57:00 by hmouhib           #+#    #+#             */
-/*   Updated: 2023/10/02 03:43:27 by hmouhib          ###   ########.fr       */
+/*   Updated: 2023/10/02 04:52:42 by hmouhib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@
 void    *ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
     size_t i;
+    const unsigned char * _src;
+    unsigned char * _dest:
 
     i = 0;
-    while (i < n && src[i] != c)
+    _src = (const unsigned char *)src;
+    _dest = (unsigned char *)dest;
+    while (i < n && _src[i] != c)
     {
-        dest[i] = src[i];
-        i += sizeof(dest[i]);
+        _dest[i] = _src[i];
+        i += sizeof(_dest[i]);
     }
     return (dest);
 }
