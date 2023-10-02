253bce68250fd1f85797e062d54a39d27206f20f
hmouhib <hmouhib@student.42.fr>
2023-10-02 04:58:22 +0100

    unsigned mok

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hmouhib <hmouhib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 03:54:28 by hmouhib           #+#    #+#             */
-/*   Updated: 2023/10/02 03:43:27 by hmouhib          ###   ########.fr       */
+/*   Updated: 2023/10/02 04:57:20 by hmouhib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 
 void    *ft_memcpy(void *dest, const void *src, size_t n)
 {
-    size_t i;
+    unsigned char   *_dest;
+    unsigned char   *_src;
+    size_t          i;
 
     i = 0;
+    _src = (unsigned char *)src;
+    _dest = (unsigned char *)dest;
     while (i < n)
     {
-        dest[i] = src[i];
-        i += sizeof(dest[i]);
+        _dest[i] = _src[i];
+        i += sizeof(_dest[i]);
     }
     return (dest);
 }
